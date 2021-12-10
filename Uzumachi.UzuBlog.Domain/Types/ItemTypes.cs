e6a75b3c0906dc4e9df795442de087fccea911ca
Uzumachi02 <uzumachi02@gmail.com>
2021-12-10 22:20:26 +0200

    add types

---
+++ Uzumachi.UzuBlog.Domain/Types/ItemTypes.cs
@@ -0,0 +1,28 @@
+﻿namespace Uzumachi.UzuBlog.Domain.Types;
+
+public static class ItemTypes {
+
+  public static readonly int User = 1;
+
+  public static readonly int Post = 2;
+
+  public static readonly int Category = 3;
+
+  public static readonly int Like = 4;
+
+  public static readonly int Comment = 5;
+
+  public static readonly int Tag = 6;
+
+  public static readonly int Ip = 7;
+
+  public static readonly int UserAgent = 8;
+
+  public static readonly int Seo = 9;
+
+  public static readonly int Page = 10;
+
+  public static readonly int Image = 11;
+
+  public static readonly int Menu = 12;
+}
---
+++ Uzumachi.UzuBlog.Domain/Types/MenuTypes.cs
@@ -0,0 +1,13 @@
+﻿namespace Uzumachi.UzuBlog.Domain.Types;
+
+public class MenuTypes {
+
+  public static readonly int Header = 1;
+
+  public static readonly int FooterInformations = 2;
+
+  public static readonly int FooterCategories = 3;
+
+  public static readonly int FooterSocials = 4;
+}
+
