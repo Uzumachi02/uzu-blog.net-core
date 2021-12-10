ec7bd03d8d2200ac20820d8589c30b792eb67c51
Uzumachi02 <uzumachi02@gmail.com>
2021-12-10 18:38:53 +0200

    Add project files.

---
+++ Uzumachi.UzuBlog.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Uzumachi.UzuBlog.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
