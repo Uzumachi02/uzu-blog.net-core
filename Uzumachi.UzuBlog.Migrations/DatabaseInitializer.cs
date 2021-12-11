fe060f36665c97b7ad760f920f00ad9ec5a53b3b
Uzumachi02 <uzumachi02@gmail.com>
2021-12-12 01:32:25 +0200

    add migrations solution

---
+++ Uzumachi.UzuBlog.Migrations/DatabaseInitializer.cs
@@ -0,0 +1,27 @@
+﻿using System.Reflection;
+using DbUp;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Uzumachi.UzuBlog.Migrations;
+
+public class DatabaseInitializer {
+
+  public static void Migrations(IServiceProvider serviceProvider) {
+    using var scope = serviceProvider.CreateScope();
+
+    IConfiguration configuration = scope.ServiceProvider.GetService<IConfiguration>();
+
+    var upgrader =
+        DeployChanges.To
+          .PostgresqlDatabase(configuration.GetConnectionString("DefaultConnection"))
+          .WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly())
+          .LogToConsole()
+          .Build();
+
+    if( upgrader.IsUpgradeRequired() ) {
+      var result = upgrader.PerformUpgrade();
+    }
+  }
+}
+
---
+++ Uzumachi.UzuBlog.Migrations/Scripts/createMigrate.fsx
@@ -0,0 +1,10 @@
+﻿// Example: dotnet fsi .\createMigrate.fsx [FILENAME]
+
+open System
+open System.IO
+
+let args = Environment.GetCommandLineArgs()
+let now = DateTime.Now
+let filename = now.ToString("yyyyMMddHHmmss")
+
+File.Create($"{filename}_{args.[2]}.sql")
