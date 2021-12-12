54add34c157d195f813ab94840ba00c0d203b39b
Александр <uzumachi02@gmail.com>
2021-12-12 23:14:17 +0200

    add post service

---
+++ Uzumachi.UzuBlog.Core/Services/PostService.cs
@@ -0,0 +1,15 @@
+﻿using Uzumachi.UzuBlog.Core.Interfaces;
+using Uzumachi.UzuBlog.Data.Interfaces;
+
+namespace Uzumachi.UzuBlog.Core.Services;
+
+public class PostService : IPostService {
+
+  private readonly IUnitOfWork _unitOfWork;
+
+  public PostService(IUnitOfWork unitOfWork) =>
+    _unitOfWork = unitOfWork;
+
+
+}
+
