d1e242cfba4456a7aecd33b4f765db5932df1fd4
Uzumachi02 <uzumachi02@gmail.com>
2021-12-14 18:11:38 +0200

    sort item types

--- Uzumachi.UzuBlog.Domain/Types/ItemTypes.cs
+++ Uzumachi.UzuBlog.Domain/Types/ItemTypes.cs
@@ -4,25 +4,25 @@ public static class ItemTypes {
 
   public static readonly int User = 1;
 
-  public static readonly int Post = 2;
+  public static readonly int Category = 2;
 
-  public static readonly int Category = 3;
+  public static readonly int Page = 3;
 
-  public static readonly int Like = 4;
+  public static readonly int Post = 4;
 
-  public static readonly int Comment = 5;
+  public static readonly int Image = 5;
 
-  public static readonly int Tag = 6;
+  public static readonly int Like = 6;
 
-  public static readonly int Ip = 7;
+  public static readonly int Comment = 7;
 
-  public static readonly int UserAgent = 8;
+  public static readonly int Tag = 8;
 
   public static readonly int Seo = 9;
 
-  public static readonly int Page = 10;
+  public static readonly int Ip = 10;
 
-  public static readonly int Image = 11;
+  public static readonly int UserAgent = 11;
 
   public static readonly int Menu = 12;
 }
