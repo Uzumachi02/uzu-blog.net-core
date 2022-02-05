57588a82141e1f59e5ff6edd0c1c74fb2638379b
Uzumachi02 <uzumachi02@gmail.com>
2022-02-05 20:59:30 +0200

    add new admin template

---
+++ Uzumachi.UzuBlog.Admin/wwwroot/js/scripts.js
@@ -0,0 +1,27 @@
+﻿var Gl = function () {
+  if (document.getElementsByClassName("js-simplebar")[0]) {
+    var e = new SimpleBar(document.getElementsByClassName("js-simplebar")[0]);
+    document.querySelectorAll(".js-sidebar [data-bs-parent]").forEach((function (t) {
+      t.addEventListener("shown.bs.collapse", (function () {
+        e.recalculate()
+      })), t.addEventListener("hidden.bs.collapse", (function () {
+        e.recalculate()
+      }))
+    }))
+  }
+},
+  Ul = function () {
+    var e = document.getElementsByClassName("js-sidebar")[0],
+      t = document.getElementsByClassName("js-sidebar-toggle")[0];
+    e && t && t.addEventListener("click", (function () {
+      e.classList.toggle("collapsed"), e.addEventListener("transitionend", (function () {
+        window.dispatchEvent(new Event("resize"))
+      }))
+    }))
+  };
+
+function initSidebar() {
+  console.log('initSidebar');
+  Gl();
+  Ul();
+}
